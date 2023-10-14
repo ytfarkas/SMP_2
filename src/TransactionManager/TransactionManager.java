dcc5422468134b6406e0d038c33a0cf2072289aa
ytfarkas <ytfarkas@gmail.com>
2023-10-14 17:57:20 -0400

    TranMan updated

--- src/TransactionManager/TransactionManager.java
+++ src/TransactionManager/TransactionManager.java
@@ -16,82 +16,175 @@ public class TransactionManager {
                 System.out.println("Transaction Manager is terminated.");
                 break;
             }
-            switch (command){
-                case "O":
+            processCommand(account, command, input);
+        }
+    }
+
+    public void processCommand (AccountDatabase account, String command, String input){
+        switch (command){
+            case "O":
+                if (openAccount(input) != null) {
                     account.open(openAccount(input));
-                    break;
-                case "C":
+                }
+                break;
+            case "C":
+                if (closeAccount(input) != null) {
                     account.close(closeAccount(input));
-                    break;
-                case "D":
-                    account.deposit(deposit(input));
-                    break;
-                case "W":
-                    account.withdraw(withdraw(input));
-                    break;
-                case "P":
-                    account.printSorted();
-                    break;
-                case "PI":
-                    account.printFeesAndInterests();
-                    break;
-                case "UB":
-                    account.printUpdatedBalances();
+                }
+                break;
+            case "D":
+                account.deposit(deposit(input));
+                break;
+            case "W":
+                account.withdraw(withdraw(input));
+                break;
+            case "P":
+                account.printSorted();
+                break;
+            case "PI":
+                account.printFeesAndInterests();
+                break;
+            case "UB":
+                account.printUpdatedBalances();
+                break;
+            default:
+                System.out.println("Invalid Command");
+        }
+    }
+    public Account openAccount(String input) {
+        String[] inputArray = input.trim().split("\\s+");
+        try {
+                String accountName = inputArray[0];
+                boolean loyalty = false;
+                switch (accountName) {
+                    case ("C"):
+                       return openChecking(input);
+                    case ("CC"):
+                        return openCollegeChecking(input);
+                    case ("S"):
+                        return openSavings(input);
+                    case ("MM"):
+                       return openMoneyMarket(input);
+                    default:
+                        System.out.println("Invalid Account"); //fix the invalid exception
+                }
+            }
+            catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Missing data for opening an account.");
+            }
+            return null; // return null?
+        }
+
+        public Account openChecking(String input) {
+            try {
+                String[] inputArray = input.trim().split("\\s+");
+                String fname = inputArray[1];
+                String lname = inputArray[2];
+                Date dob = new Date(inputArray[3]);
+                Profile profile = new Profile(fname, lname, dob);
+                double bal = Double.parseDouble(inputArray[4]);
+                return new Checking(profile, bal);
+            }
+            catch(ArrayIndexOutOfBoundsException e){
+                System.out.println("Missing data for opening an account.");
+            }
+            catch (NumberFormatException e){
+                System.out.println("Not a valid amount.");
+            }
+            return null;
+        }
+
+        public Account openCollegeChecking(String input) {
+        try {
+            String[] inputArray = input.trim().split("\\s+");
+            String fname = inputArray[1];
+            String lname = inputArray[2];
+            Date dob = new Date(inputArray[3]);
+            Profile profile = new Profile(fname, lname, dob);
+            double bal = Double.parseDouble(inputArray[4]);
+            int campusCode = Integer.parseInt(inputArray[5]);
+            return new CollegeChecking(profile, bal, campusCode);
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Missing data for opening an account.");
+        }
+        catch (NumberFormatException e){
+            System.out.println("Not a valid amount.");
+        }
+        return null;
+    }
+
+    public Account openSavings(String input) {
+        try {
+            String[] inputArray = input.trim().split("\\s+");
+            String fname = inputArray[1];
+            String lname = inputArray[2];
+            Date dob = new Date(inputArray[3]);
+            Profile profile = new Profile(fname, lname, dob);
+            double bal = Double.parseDouble(inputArray[4]);
+            boolean savingLoyalty = false;
+            if (inputArray[5].equals("1")) {
+               savingLoyalty = true;
+            }
+            return new Savings(profile, bal, savingLoyalty);
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Missing data for opening an account.");
+        }
+        catch (NumberFormatException e){
+            System.out.println("Not a valid amount.");
+        }
+
+        return null;
+    }
+
+    public Account openMoneyMarket(String input) {
+        try {
+            String[] inputArray = input.trim().split("\\s+");
+            String fname = inputArray[1];
+            String lname = inputArray[2];
+            Date dob = new Date(inputArray[3]);
+            Profile profile = new Profile(fname, lname, dob);
+            double bal = Double.parseDouble(inputArray[4]);
+            return new MoneyMarket(profile, bal);
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Missing data for opening an account.");
+        }
+        catch (NumberFormatException e){
+            System.out.println("Not a valid amount.");
+        }
+        return null;
+    }
+
+
+
+    public Account closeAccount(String input){
+        try {
+            String[] inputArray = input.trim().split("\\s+");
+            String accountName = inputArray[0];
+            String fname = inputArray[1];
+            String lname = inputArray[2];
+            Date dob = new Date(inputArray[3]);
+            Profile profile = new Profile(fname, lname, dob);
+            switch (accountName) {
+                case ("C"):
+                    return new Checking(profile);
+                case ("CC"):
+                    return new CollegeChecking(profile);
+                case ("S"):
+                    return new Savings(profile);
+                case ("MM"):
+                    return new MoneyMarket(profile);
+                default:
+                    System.out.println("Invalid Account"); //fix the invalid exception
                     break;
             }
         }
-    }
-    public Account openAccount(String input){
-        String[] inputArray = input.trim().split("\\s+");
-        String accountName = inputArray[0];
-        String fname = inputArray[1];
-        String lname = inputArray[2];
-        Date dob = new Date(inputArray[3]);
-        Profile profile = new Profile(fname, lname, dob);
-        double bal = Double.parseDouble(inputArray[4]);
-        boolean loyalty = false;
-        switch(accountName) {
-            case ("C"):
-                return new Checking(profile, bal);
-            case ("CC"):
-                int campusCode = Integer.parseInt(inputArray[5]);
-                return new CollegeChecking(profile, bal, campusCode);
-            case ("S"):
-                if (bal >= 2000) {
-                    loyalty = true;
-                }
-                return new Savings(profile, bal, loyalty);
-            case ("MM"):
-                return new MoneyMarket(profile, bal, loyalty, 0);
-            default:
-                System.out.println("Invalid Account"); //fix the invalid exception
-                break;
-        }
-        return null; // return null?
-    }
-
-    public Account closeAccount(String input){
-        String[] inputArray = input.trim().split("\\s+");
-        String accountName = inputArray[0];
-        String fname = inputArray[1];
-        String lname = inputArray[2];
-        Date dob = new Date(inputArray[3]);
-        Profile profile = new Profile(fname, lname, dob);
-        switch(accountName) {
-            case ("C"):
-                return new Checking(profile);
-            case ("CC"):
-                return new CollegeChecking(profile);
-            case ("S"):
-                return new Savings(profile);
-            case ("MM"):
-                return new MoneyMarket(profile);
-            default:
-                System.out.println("Invalid Account"); //fix the invalid exception
-                break;
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Missing data for closing an account.");
         }
         return null; //return null?
-
     }
 
     public Account deposit(String input){
